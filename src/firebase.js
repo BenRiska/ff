31ff158cff328636b3f7c894ba1529a9e27728ad
benriska <ben10taylor10@hotmail.com>
2020-10-01 09:32:53 +0100

    start

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjb8rZQ3U0Ii-jIby2wCIcpmS6d44q5hI",
+  authDomain: "featurefield-9a72e.firebaseapp.com",
+  databaseURL: "https://featurefield-9a72e.firebaseio.com",
+  projectId: "featurefield-9a72e",
+  storageBucket: "featurefield-9a72e.appspot.com",
+  messagingSenderId: "111578997590",
+  appId: "1:111578997590:web:48d0dda475856d94c2ecf7",
+  measurementId: "G-EZEL5T6DXB",
+};
